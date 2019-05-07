bbc047a14b674fe4742f5404540068dda2b83196
roger <rogermartinsdesign@gmail.com>
2019-05-07 15:29:31 -0400

    implementando a lógica 7-4

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -27,11 +27,50 @@ export default class Calculator extends Component {
   }
 
   setOperation(operation) {
-    console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      //quando o usuário finalizar a operação com igual
+      const equals = operation === '='
+      //pegando a 1ª operação e não uma nova caso o usuário digite outra em seguida
+      const currentOperation = this.state.operation
+      
+      //clone de values para podermos operar
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+      } catch (error) {
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   addDigit(n) {
-    console.log(n);
+    //Evitar que tenha dois pontos no mesmo número
+    if (n === '.' && this.state.displayValue.includes('.')){
+      return
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.'){
+      const i = this.state.current
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[i] = newValue
+      this.setState({ values })
+    }
   }
 
   render() {
